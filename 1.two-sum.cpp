ec06b6514eccd9faf8625ede474b5e5ebd83fafe
Mingjing Yu <mingjingyu@microsoft.com>
2022-01-24 17:02:58 +0800

    init

---
+++ 1.two-sum.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
